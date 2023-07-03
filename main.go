32a2aa2baee8db1e17f851d5c54c4f4fe655da57
Sean Bain <artemis45566@gmail.com>
2023-07-04 00:08:38 +0800

    environment variables

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
 }
